def964ee05dac39746675e7d24e05200a7a53127
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-22 14:39:31 +0530

    added agec result form and write functionalities

---
+++ frontend/src/components/AgeResult.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import '../App.css'
+import PropTypes from 'prop-types'
+
+const AgeResult = ({age}) => {
+  return (
+    <h4>You are {age.years} years, {age.months} months, and {age.days} days old.</h4>
+  )
+}
+AgeResult.propTypes = {
+  age: PropTypes.shape({
+    years: PropTypes.number.isRequired,
+    months: PropTypes.number.isRequired,
+    days: PropTypes.number.isRequired,
+  })
+}
+
+export default AgeResult
