394e067a5c0d70bea54ff01836988553bb4bec9c
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-22 14:38:41 +0530

    wrote functionalities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
 import './App.css'
+import AgeCalculatorForm from './components/AgeCalculatorForm'
+import AgeResult from './components/AgeResult'
+import {differenceInYears, differenceInMonths, differenceInDays} from 'date-fns'
 
 function App() {
+  const [age, setAge] = useState(null)
+  const calculateAge = (birthDate) => {
+    const today = new Date();
+    const birthDayObject = new Date(birthDate);
+    const ageYears = differenceInYears(today, birthDayObject)
+    const ageMonths = differenceInMonths(today, birthDayObject)
+    const ageDays = differenceInDays(today, birthDayObject)
+    console.log(ageYears+" - "+ageMonths+" - "+ageDays)
+
+    setAge({
+      years: ageYears,
+      months: ageMonths,
+      days: ageDays,
+    })
+  }
 
   return (
     <div>
-      Calculator
+      <h1>Age Calculator</h1>
+      <AgeCalculatorForm calculateAge={calculateAge} />
+      {age && <AgeResult age={age} />}
+
     </div>
   )
 }
